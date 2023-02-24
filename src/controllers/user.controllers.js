9473e7674cba4735d84c59238f69076dde375ba0
ftorrelles <torrellesf93@gmai.com>
2023-02-23 20:01:26 -0400

    change

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,55 @@
+const catchError = require("../utils/catchError");
+const User = require("../models/User");
+//get all
+const getAll = catchError(async (req, res) => {
+    const users = await User.findAll();
+    return res.json(users);
+});
+// create
+const create = catchError(async (req, res) => {
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const user = await User.create({
+        first_name,
+        last_name,
+        email,
+        password,
+        birthday,
+    });
+    return res.status(201).json(user);
+});
+//get one
+const getOne = catchError(async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    return res.json(user);
+});
+//delete
+const remove = catchError(async (req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: { id } });
+    return res.sendStatus(204);
+});
+// update
+const update = catchError(async (req, res) => {
+    const { id } = req.params;
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const user = await User.update(
+        {
+            first_name,
+            last_name,
+            email,
+            password,
+            birthday,
+        },
+        { where: { id }, returning: true }
+    );
+    return res.json(user[1][0]);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update,
+};
